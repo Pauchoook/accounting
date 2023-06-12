1579cf8a099a75b2ceb9bb909ae53c3055176c7d
Pauchoook <pauchoook@mail.ru>
2023-06-12 16:56:22 +0300

    our-service

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,8 @@ import burger from "./files/burger.js";
 import dropdown from "./files/dropdown.js";
 import * as flsFunctions from "./files/functions.js";
 import sliders from './files/sliders.js';
+import spoller from "./files/spoller.js";
 
 burger();
 dropdown();
+spoller();
